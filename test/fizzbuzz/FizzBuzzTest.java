3ebd542fd553efdb0670e6487666030d302a0f96
yamaguchi.ryo <yamaguchi.ryo@m.alhinc.jp>
2020-05-08 09:36:31 +0900

    third commit

--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -1,99 +1,51 @@
-/**
- *
- */
 package fizzbuzz;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-/**
- * @author お客様
- *
- */
 public class FizzBuzzTest {
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	/**
 	 * {@link fizzbuzz.FizzBuzz#fizzBuzzCheck(int)}
 	 * のためのテスト・メソッド。(引数が〇の時の対処のテスト)
 	 */
 	@Test
-	public void testFizzBuzzCheck_01() {
-		System.out.println("testFizzBuzzCheck_01");
+	public void inputFizzBuzzValue() {
 		assertEquals("FizzBuzz", FizzBuzz.fizzBuzzCheck(15));
 	}
 
 	@Test
-	public void testFizzBuzzCheck_02() {
-		System.out.println("testFizzBuzzCheck_02");
-		assertEquals("Buzz", FizzBuzz.fizzBuzzCheck(5));
-	}
-
-	@Test
-	public void testFizzBuzzCheck_03() {
-		System.out.println("testFizzBuzzCheck_03");
-		assertEquals("Fizz", FizzBuzz.fizzBuzzCheck(3));
-	}
-
-	@Test
-	public void testFizzBuzzCheck_04() {
-		System.out.println("testFizzBuzzCheck_04");
+	public void inputDoubleFizzBuzzValue() {
 		assertEquals("FizzBuzz", FizzBuzz.fizzBuzzCheck(30));
 	}
 
 	@Test
-	public void testFizzBuzzCheck_05() {
-		System.out.println("testFizzBuzzCheck_05");
+	public void inputBuzzValue() {
+		assertEquals("Buzz", FizzBuzz.fizzBuzzCheck(5));
+	}
+
+	@Test
+	public void inputDoubleBuzzValue() {
 		assertEquals("Buzz", FizzBuzz.fizzBuzzCheck(10));
 	}
 
 	@Test
-	public void testFizzBuzzCheck_06() {
-		System.out.println("testFizzBuzzCheck_06");
+	public void inputFizzValue() {
+		assertEquals("Fizz", FizzBuzz.fizzBuzzCheck(3));
+	}
+
+	@Test
+	public void inputDoubleFizzValue() {
 		assertEquals("Fizz", FizzBuzz.fizzBuzzCheck(6));
 	}
 
 	@Test
-	public void testFizzBuzzCheck_07() {
-		System.out.println("testFizzBuzzCheck_07");
+	public void inputBoundaryValue() {
 		assertEquals("4", FizzBuzz.fizzBuzzCheck(4));
 	}
 
 	@Test
-	public void testFizzBuzzCheck_08() {
-		System.out.println("testFizzBuzzCheck_08");
+	public void inputDoubleBoundaryValue() {
 		assertEquals("8", FizzBuzz.fizzBuzzCheck(8));
 	}
 }
